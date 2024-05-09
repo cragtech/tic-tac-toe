c7b094e577021831dafb4dbe0e210e80a0787375
cragtech <cragtechnology@gmail.com>
2024-05-08 23:43:41 -0600

    Player border style switching function added and functioning based on which player's turn it is.

--- src/App.js
+++ src/App.js
@@ -7,10 +7,15 @@ function App() {
   const [player2, setPlayer2] = useState('Player 2');
   const player1Char = 'X';
   const player2Char = 'O';
+  const playerBorderRed = 'solid 3px red';
+  const playerBorderNone = 'none';
+  const [player1BorderStyle, setPlayer1BorderStyle] = useState(playerBorderRed);
+  const [player2BorderStyle, setPlayer2BorderStyle] = useState(playerBorderNone);
   const [player1Score, setPlayer1Score] = useState(0);
   const [player2Score, setPlayer2Score] = useState(0);
   const [squareChar, setSquareChar] = useState(player1Char);
   const [squares, setSquares] = useState(['', '', '', '', '', '', '', '', '']);
+
 
 
   const squareClick = (indexVal) => {
@@ -19,8 +24,12 @@ function App() {
     setSquares([...squaresArray]);
     if (squareChar === player1Char) {
       setSquareChar(player2Char);
+      setPlayer2BorderStyle(playerBorderNone);
+      setPlayer1BorderStyle(playerBorderRed);
     } else {
     setSquareChar(player1Char);
+    setPlayer1BorderStyle(playerBorderNone);
+    setPlayer2BorderStyle(playerBorderRed);
     }
   }
 
@@ -69,14 +78,16 @@ function App() {
       </div>
       <div id="players-box">
         <input
-          className="players-input"
+          id="player1-input"
           type="text" value={player1}
+          style={{ border: player1BorderStyle }}
           onChange={(e) => setPlayer1(e.target.value)}
         />
         vs
         <input
-          className="players-input"
+          id="player2-input"
           type="text" value={player2}
+          style={{ border: player2BorderStyle }}
           onChange={(e) => setPlayer1(e.target.value)}
         />
       </div>
